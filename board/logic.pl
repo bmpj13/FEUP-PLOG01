86df7285e657ff09ebd0d3ffd2409d211ec46065
João Barbosa <bmpj13@hotmail.com>
2016-11-10 11:55:17 +0000

    Modulating

--- board/logic.pl
+++ board/logic.pl
@@ -19,6 +19,11 @@ emptyPosition(Px,Py,Board,NBoard) :-
 	(setBoardCell(Px, Py, square, Board, NBoard)).
 
 
+hasWalls(Player) :-
+	wallNumber(Player, H, V),
+	(H =\= 0 ; V =\= 0).
+
+
 
 moveOneSpace(Pawn, X, Y, Board, NewBoard) :-
 	targetCoords(Pawn, X, Y, Nx, Ny),
@@ -59,3 +64,98 @@ targetIsValid(_, _, _, _) :-
 inBounds(Tx, Ty) :-
 	Tx >= 0, Tx < 21,
 	Ty >= 0, Ty < 27.
+
+
+
+placeWall(Player, _, _, 'h', _, _) :-
+	wallNumber(Player, 0, _), !,
+	write('---- You\'re out of horizontal walls ----'), nl,
+	fail.
+
+
+placeWall(Player, _, _, 'v', _, _) :-
+	wallNumber(Player, _, 0), !,
+	write('---- You\'re out of vertical walls ----'), nl,
+	fail.
+
+
+placeWall(Player,X, Y,'h',Board, NewBoard) :-
+	checkWallCoords(X, Y,'h',Board),
+	retract(wallNumber(Player, H, V)),
+	Nh is H - 1,
+	assert(wallNumber(Player, Nh, V)),
+	Nx is round(X/2),
+  setBoardCell(Nx, Y, [horizontal, placed], Board, AuxBoard),
+	Nx2 is Nx + 1,
+	setBoardCell(Nx2, Y, [horizontal, placed], AuxBoard, NewBoard).
+
+
+placeWall(Player,X, Y,'v',Board, NewBoard) :-
+	checkWallCoords(X, Y,'v',Board),
+	retract(wallNumber(Player, H, V)),
+	Nv is V - 1,
+	assert(wallNumber(Player, H, Nv)),
+  setBoardCell(X, Y, [vertical, placed], Board, AuxBoard),
+	Ny is Y + 2,
+	setBoardCell(X, Ny, [vertical, placed], AuxBoard, NewBoard).
+
+
+
+checkWallCoords(X, Y,'v',Board) :-
+	X mod 2 =:= 1,
+	Y mod 2 =:= 0,
+	X < 20, X > 0,
+	Y < 25, Y >= 0,
+	noWallPlaced(Board, X, Y, 'v'),
+	Cx is X - 1,
+	Cy is Y + 1,
+	noWallPlaced(Board, Cx, Cy, 'h').
+
+
+checkWallCoords(X, Y,'h',Board) :-
+	X mod 2 =:= 0,
+	Y mod 2 =:= 1,
+	X >= 0, X < 19,
+	Y > 0, Y < 26,
+	noWallPlaced(Board, X, Y, 'h'),write('ola1'),
+	Cx is X + 1,
+	Cy is Y - 1,
+	noWallPlaced(Board, Cx, Cy, 'v'), write('ola2').
+
+
+checkWallCoords(_,_,_,_) :-
+	write('---- Invalid wall coordenates ----'),nl,
+	fail.
+
+
+
+noWallPlaced(Board, X, Y, 'h') :-
+  Nx is round(X/2),
+  elementCoords(Board, Nx, Y, Elem1),
+  Nx2 is Nx + 1,
+  elementCoords(Board, Nx2, Y, Elem2),
+  (Elem1 = [_, empty] ; Elem2 = [_, empty]).
+
+
+noWallPlaced(Board, X, Y, 'v') :-
+  elementCoords(Board, X, Y, Elem1),
+  Ny is Y + 2,
+  elementCoords(Board, X, Ny, Elem2),
+  (Elem1 = [_, empty] ; Elem2 = [_, empty]).
+
+
+
+%posicao da parede que tem que ser atravessada para passar para a proxima posição
+% x,y é o offset px,py posicao do jogador wx, wy é o retorno com as coordenadas da parede
+wallCoords(X,Y,Px,Py,Wx,Wy) :-
+  (X =:= 0 , Wx is round(Px/2) , Wy is Py + Y);
+  (Y =:= 0 , Wx is Px + X , Wy is Py).
+
+
+checkWallColision([_ | [placed | _ ]]) :-
+	write('----There is a wall in the way you have to choose another path!----'),nl,
+	fail.
+
+
+checkWallColision([_ | [empty | _]]) :-
+	true.
--- plays.pl
+++ plays.pl
@@ -9,6 +9,7 @@ play(M, L, Player, Board, NewBoard) :-
 	(M =:= 3, playBot(L, Player, Board, NewBoard)).
 
 
+
 playHuman(Player, Board, NewBoard) :-
 	format('--------------Its your turn ~s ----------- ~n',[Player]),
 	getPawnNumber(N),
@@ -24,6 +25,27 @@ playBot(L, Player,Board,NewBoard):-
 moveHuman(Pawn, Board, NewBoard) :-
 	moveOneSpaceHuman(Pawn,Board,AuxBoard),
 	moveOneSpaceHuman(Pawn,AuxBoard,NewBoard).
+
+
+
+handleWall(Player, Board, NewBoard) :-
+	hasWalls(Player), !,
+	repeat,
+		once(getWallCoords(X, Y, O)),		% posicionar a parede
+	placeWall(Player, X, Y, O, Board, NewBoard). 	%ver se o numero de paredes for 0 não perguntar as coordenadas
+
+handleWall(_, Board, Board) :-
+	true.
+
+
+moveOneSpaceHuman(Pawn,Board,NewBoard) :-
+	repeat, %verificar colisoes
+		once(getMovCoords(X,Y)),
+	validPosition(Pawn, Board, X, Y),
+	moveOneSpace(Pawn, X, Y, Board, NewBoard),
+	displayBoard(NewBoard).
+
+
 
 moveBot(1, Player, Board, NewBoard) :-
 	evaluateBestPawn(Player,N),
@@ -49,7 +71,29 @@ handleWallBot(2, Player, Board, NewBoard) :-
 	hasWalls(Player), !,
 	repeat,
 		once(randomWall(X, Y, O)),
-		placeWall(Player, X, Y, O, Board, NewBoard).
+	placeWall(Player, X, Y, O, Board, NewBoard).
+
+
+
+auxMoveBot(Player,N,Board,NewBoard) :-
+	evaluateBestDirection(Player,N,Directions),
+	moveOneSpaceBot([Player,N],Directions,Board,NewBoard).
+
+
+
+moveOneSpaceBot(Pawn,Directions,Board,NewBoard) :-
+	iterateDirectionList(Directions,X,Y,Pawn,Board),
+	moveOneSpace(Pawn, X, Y, Board, NewBoard),
+	displayBoard(NewBoard).
+
+
+
+iterateDirectionList([[_|D] | _], X, Y, Pawn, Board) :-
+	convertDirection(D,X,Y),
+	validPosition(Pawn, Board, X, Y), !.
+
+iterateDirectionList([[_|_] | Res], X, Y, Pawn, Board) :-
+	iterateDirectionList(Res,X,Y,Pawn,Board).
 
 
 
@@ -62,18 +106,6 @@ iterateWallList([[X,Y,O] | _], Player, Board, NewBoard) :-
 
 iterateWallList([[_,_,_] | Res], Player, Board, NewBoard) :-
 	iterateWallList(Res,Player,Board,NewBoard).
-
-
-
-
-handleWall(Player, Board, NewBoard) :-
-	hasWalls(Player), !,
-	repeat,
-		once(getWallCoords(X, Y, O)),		% posicionar a parede
-	placeWall(Player, X, Y, O, Board, NewBoard). 	%ver se o numero de paredes for 0 não perguntar as coordenadas
-
-handleWall(_, Board, Board) :-
-	true.
 
 
 
@@ -105,125 +137,3 @@ checkBotWin(Player,N) :-
 	targePosition([Player, 2], Tx2, _),
 	(X =:= Tx1 ; X =:= Tx2),
 	 Y =:= Ty1.
-
-
-auxMoveBot(Player,N,Board,NewBoard) :-
-	evaluateBestDirection(Player,N,Directions),
-	moveOneSpaceBot([Player,N],Directions,Board,NewBoard).
-
-
-moveOneSpaceBot(Pawn,Directions,Board,NewBoard) :-
-	iterateDirectionList(Directions,X,Y,Pawn,Board),
-	moveOneSpace(Pawn, X, Y, Board, NewBoard),
-	displayBoard(NewBoard).
-
-iterateDirectionList([[_|D] | _], X, Y, Pawn, Board) :-
-	convertDirection(D,X,Y),
-	validPosition(Pawn, Board, X, Y), !.
-
-iterateDirectionList([[_|_] | Res], X, Y, Pawn, Board) :-
-	iterateDirectionList(Res,X,Y,Pawn,Board).
-
-
-moveOneSpaceHuman(Pawn,Board,NewBoard) :-
-	repeat, %verificar colisoes
-		once(getMovCoords(X,Y)),
-	validPosition(Pawn, Board, X, Y),
-	moveOneSpace(Pawn, X, Y, Board, NewBoard),
-	displayBoard(NewBoard).
-
-
-
-%posicao da parede que tem que ser atravessada para passar para a proxima posição
-% x,y é o offset px,py posicao do jogador wx, wy é o retorno com as coordenadas da parede
-wallCoords(X,Y,Px,Py,Wx,Wy) :-
-	(X =:= 0 , Wx is round(Px/2) , Wy is Py + Y);
-	(Y =:= 0 , Wx is Px + X , Wy is Py).
-
-checkWallColision([_ | [placed | _ ]]) :-
-					write('----There is a wall in the way you have to choose another path!----'),nl,
-					fail.
-
-checkWallColision([_ | [empty | _]]) :-
-					true.
-
-
-
-hasWalls(Player) :-
-	wallNumber(Player, H, V),
-	(H =\= 0 ; V =\= 0).
-
-
-
-placeWall(Player, _, _, 'h', _, _) :-
-		wallNumber(Player, 0, _), !,
-		write('---- You\'re out of horizontal walls ----'), nl,
-		fail.
-
-
-placeWall(Player, _, _, 'v', _, _) :-
-		wallNumber(Player, _, 0), !,
-		write('---- You\'re out of vertical walls ----'), nl,
-		fail.
-
-
-placeWall(Player,X, Y,'h',Board, NewBoard) :-
-		checkWallCoords(X, Y,'h',Board),
-		retract(wallNumber(Player, H, V)),
-		Nh is H - 1,
-		assert(wallNumber(Player, Nh, V)),
-		Nx is round(X/2),
-    setBoardCell(Nx, Y, [horizontal, placed], Board, AuxBoard),
-  	Nx2 is Nx + 1,
-  	setBoardCell(Nx2, Y, [horizontal, placed], AuxBoard, NewBoard).
-
-placeWall(Player,X, Y,'v',Board, NewBoard) :-
-		checkWallCoords(X, Y,'v',Board),
-		retract(wallNumber(Player, H, V)),
-		Nv is V - 1,
-		assert(wallNumber(Player, H, Nv)),
-    setBoardCell(X, Y, [vertical, placed], Board, AuxBoard),
-  	Ny is Y + 2,
-  	setBoardCell(X, Ny, [vertical, placed], AuxBoard, NewBoard).
-
-
-checkWallCoords(X, Y,'v',Board) :-
-		X mod 2 =:= 1,
-		Y mod 2 =:= 0,
-		X < 20, X > 0,
-		Y < 25, Y >= 0,
-		noWallPlaced(Board, X, Y, 'v'),
-		Cx is X - 1,
-		Cy is Y + 1,
-		noWallPlaced(Board, Cx, Cy, 'h').
-
-
-
-checkWallCoords(X, Y,'h',Board) :-
-		X mod 2 =:= 0,
-		Y mod 2 =:= 1,
-		X >= 0, X < 19,
-		Y > 0, Y < 26,
-		noWallPlaced(Board, X, Y, 'h'),write('ola1'),
-		Cx is X + 1,
-		Cy is Y - 1,
-		noWallPlaced(Board, Cx, Cy, 'v'), write('ola2').
-
-
-checkWallCoords(_,_,_,_) :-
-		write('---- Invalid wall coordenates ----'),nl,
-		fail.
-
-noWallPlaced(Board, X, Y, 'h') :-
-	Nx is round(X/2),
-	elementCoords(Board, Nx, Y, Elem1),
-	Nx2 is Nx + 1,
-	elementCoords(Board, Nx2, Y, Elem2),
-	(Elem1 = [_, empty] ; Elem2 = [_, empty]).
-
-
-noWallPlaced(Board, X, Y, 'v') :-
-	elementCoords(Board, X, Y, Elem1),
-	Ny is Y + 2,
-	elementCoords(Board, X, Ny, Elem2),
-	(Elem1 = [_, empty] ; Elem2 = [_, empty]).
