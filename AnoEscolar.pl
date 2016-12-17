93720b367a6179c577bdb0cce87cdf47aeeaf5c0
Jose Martins <lluismmartins7@gmail.com>
2016-12-17 13:09:07 +0000

    working on tpc restrictions

--- AnoEscolar.pl
+++ AnoEscolar.pl
@@ -9,9 +9,10 @@
 % alunos não podem ter mais de 2 testes por semana ->feito
 % alunos não podem ter testes em dias consecutivos ->feito
 %  -------------- TPC --------------
-% alunos não podem ter mais de 2(Afinal é VARIAVEL) tpc por dia
-% em pelo menos um dia por semana não pode haver tpc(um dia da semana que deve ser sempre o mesmo )
+% alunos não podem ter mais de 2(Afinal é VARIAVEL) tpc por dia -> feito
+% em pelo menos um dia por semana não pode haver tpc(um dia da semana que deve ser sempre o mesmo)
 % em cada disciplina só pode haver tpc em metade das aulas(Afinal é VARIAVEL)
+% --------------- FODEU ----------------
 % testes de cada disciplina devem ser o mais proximo possivel em todas as turmas
 
 
@@ -19,17 +20,32 @@
 %solveProblem().
 %for each class call resolve class and display results
 
+%solver
 %resolve problema de uma turma
 solveClass(Days,Schedule,Disciplines,Class):-
   fillDisciplines(Days,Disciplines,Class),
   checkHasDiscipline(Class,Schedule),%coloca a 0 a lista de tpc e testes nos dias em que nao existem aulas dessa disciplina
-  %domain(NoTpcDay,1,5), % depois por todos os dias a 0 na lista de tpc no dia instanciado
+  domain([NoTpcDay],1,5), % dia da semana em que nunca há tpc
+
+  %clearTpcDay(Class,NoTpcDay),
+
+  maxNumberTpcPerDay(Class,Days,1), % alunos não podem ter mais de 2(Afinal é VARIAVEL) tpc por dia
+  %limitNumberOfTpcPerPeriod(Class,2,Schedule,Days).
+
   twoTestsPerPeriod(Class), % garantir 2 testes por periodo, FALTA POLOS NO MEIO/FIM do PERIODO
   testPlacementRestrictions(Days,Class),
+
+
+  getDaySumList(2,Class,0,Days, Tpc),
+
+
   getLabelVars(Class,[],Res),
-  write('Res'), nl, !,
-  labeling([],Res),
-  displayClass(Class,Days).
+  append([NoTpcDay],Res,Resolution),
+  write('Res'), nl,
+  labeling([],Resolution),
+  displayClass(Class,Days),
+  format('No Tpc Day: ~w ~n',[NoTpcDay]),
+  write('-----------TPC-----------'),nl,write(Tpc).
 
 
 
@@ -42,6 +58,55 @@ getLabelVars([CurrentDiscipline | NextDiscipline], CurrentList ,Res) :-
   append(CurrentList,DisciplineVars,Nres),
   getLabelVars(NextDiscipline, Nres, Res).
 
+
+%numero de aulas de uma disciplina num periodo
+
+getNumberDisciplineLessons(_,_,N,N,0).
+
+getNumberDisciplineLessons(Schedule,DisciplineId,N,Days,Res) :-
+  Index is (N mod 5),
+  nth0(Index, Schedule, L),
+  N1 is N + 1,
+  getNumberDisciplineLessons(Schedule,DisciplineId,N1,Days,R),
+  ((member(DisciplineId, L), Res is R + 1 ,!) ; (Res is R , !)).
+
+
+%tpc
+
+% em cada disciplina só pode haver tpc em metade das aulas(Afinal é VARIAVEL)
+% ratio 2 tpc no maximo em metade das aulas , ratio 3 tpc no maximo um terço das aulas
+limitNumberOfTpcPerPeriod([],_,_,_).
+
+limitNumberOfTpcPerPeriod([[DisciplineId,Test,Tpc] | Tail],Ratio,Schedule,Days):-
+  getNumberDisciplineLessons(Schedule,DisciplineId,0,Days,NumberLessons),
+  MaxTpc is div(NumberLessons,Ratio),
+  sum(Tpc,#=<,MaxTpc),
+  limitNumberOfTpcPerPeriod(Tail, Ratio, Schedule, Days).
+
+
+
+%clearTpcDay([CurrentDiscipline | NextDiscipline],NoTpcDay) :-
+%  nth1(3,CurrentDiscipline,Tpc),
+
+%clearTpcDayDiscipline(Discipline,N,NoTpcDay) :-
+%    Index is (N mod 5) + 1,
+
+
+
+
+maxNumberTpcPerDay(Class,Days,N):-
+  getDaySumList(2,Class,0,Days, Tpc), % lista com todos os dias e numero de tpc's de todas as disciplinas da turma nesse dia
+  checkTpcNumber(Tpc,N).
+
+checkTpcNumber([], _).
+
+checkTpcNumber([CurrentTpc | NextTpc], N) :-
+  CurrentTpc #=< N,
+  checkTpcNumber(NextTpc, N).
+
+
+
+%inicializacoes
 fillDisciplines(Days,[],[]).
 
 % dias id das disciplinas
@@ -80,29 +145,30 @@ twoTestsPerPeriod([CurrentDiscipline | NextDiscipline]) :-
     twoTestsPerPeriod(NextDiscipline).
 
 
+%tests
 
-getNTestsDay([], _ , 0).
+getDaySum(_, [], _ , 0).
 
-getNTestsDay([CurrentDiscipline | NextDiscipline], Id, Value) :- !,
-    nth0(1,CurrentDiscipline,Tests),
-    nth0(Id,Tests,V),
+getDaySum(IdList,[CurrentDiscipline | NextDiscipline], Id, Value) :- !,
+    nth0(IdList,CurrentDiscipline,List),
+    nth0(Id,List,V),
     Value #= V1 + V,
-    getNTestsDay(NextDiscipline, Id, V1).
+    getDaySum(IdList,NextDiscipline, Id, V1).
 
 
-getAllTestList(_, Days, Days, []).
+getDaySumList(_, _, Days, Days, []).
 
-getAllTestList(Class,N,Days, Result):- !,
-  getNTestsDay(Class, N, Val),
+%IdList 1-Test 2-Tpc
+getDaySumList(IdList,Class,N,Days, Result):- !,
+  getDaySum(IdList,Class, N, Val),
   N1 is N + 1,
-  getAllTestList(Class,N1,Days, Val2),
+  getDaySumList(IdList,Class,N1,Days, Val2),
   Result = [Val | Val2].
 
 testPlacementRestrictions(Days,Class) :-
-  getAllTestList(Class,0,Days, Tests), % lista com todos os dias e numero de testes de todas as disciplinas da turma nesse dia
+  getDaySumList(1,Class,0,Days, Tests), % lista com todos os dias e numero de testes de todas as disciplinas da turma nesse dia
   checkWeekTestNumber(Tests,2),
   checkConsecutiveDayTests(Tests).
-
 
 
 checkConsecutiveDayTests([]).
